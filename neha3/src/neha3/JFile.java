2e9b2e3e31b2afd23d16eb5eaea2bece5d638394
nmittal020695 <65939616+nmittal020695@users.noreply.github.com>
2020-06-03 15:50:27 +0530

    file added

---
+++ neha3/src/neha3/JFile.java
@@ -0,0 +1,28 @@
+package neha3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JFile {
+
+	
+ 
+			@Before
+			public void beforeTest(){
+			System.out.println("Running before test");
+			}
+			 
+			@Test
+			public void junitTest(){
+			System.out.println("Running Junit test");
+			}
+			 
+			@Test
+			public void secondJunitTest(){
+			System.out.println("Running second Junit test");
+			}
+			}
+		
+
